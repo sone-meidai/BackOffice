03084a085c296bd92afc3cc598f80378909f6264
SONE-Mei <185347250+sone-meidai@users.noreply.github.com>
2025-07-14 19:37:16 +0700

    init shadcn

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,12 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2025-05-15",
   devtools: { enabled: true },
-  modules: ["@nuxt/eslint"],
+  modules: ["@nuxt/eslint", "shadcn-nuxt"],
+  css: ["~/assets/css/tailwind.css"],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
