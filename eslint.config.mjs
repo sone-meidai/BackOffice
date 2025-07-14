bd96ec11462e9f8a23f4cc45178ea60c3b0266de
SONE-Mei <185347250+sone-meidai@users.noreply.github.com>
2025-07-14 19:31:42 +0700

    config prettier + eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,18 @@
-// @ts-check
-import withNuxt from './.nuxt/eslint.config.mjs'
+import withNuxt from "./.nuxt/eslint.config.mjs";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 
-export default withNuxt(
-  // Your custom configs here
-)
+export default withNuxt(eslintPluginPrettierRecommended, {
+  languageOptions: {
+    parserOptions: {
+      parser: "@typescript-eslint/parser",
+      tsconfigRootDir: import.meta.dirname,
+    },
+  },
+  rules: {
+    "no-debugger": "error",
+    "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_", vars: "all", varsIgnorePattern: "^_" }],
+    "@typescript-eslint/no-duplicate-enum-values": "error",
+    "@typescript-eslint/no-explicit-any": "error",
+    "func-style": ["error", "expression", { allowArrowFunctions: true }],
+  },
+});
